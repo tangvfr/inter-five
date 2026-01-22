7b241f04e1b145fb62162bf64cc6cfe69d277a1d
tangvfr <tangv.minecraft@gmail.com>
2026-01-22 11:39:21 +0100

    :truck: Rename package of authentification

--- authentification/src/main/java/fr/miage/orleans/m2/interop/tp/authentification/AuthentificationApplication.java
+++ authentification/src/main/java/fr/miage/orleans/m2/interop/tp/authentification/AuthentificationApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthentificationApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(AuthentificationApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(AuthentificationApplication.class, args);
+    }
+
 }
