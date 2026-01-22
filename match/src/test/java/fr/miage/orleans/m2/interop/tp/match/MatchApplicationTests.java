7eeaf986fd3d9c2a928af9a1f3634084ac892852
tangvfr <tangv.minecraft@gmail.com>
2026-01-22 11:20:16 +0100

    :construction: Create Match module

---
+++ match/src/test/java/fr/miage/orleans/m2/interop/tp/match/MatchApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.orleans.m2.interop.tp.match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
